73017eda663a2f385ed0658409e13df4d349caf2
Muhammad Usman Latif <usmanlatif603@gmail.com>
2022-05-27 13:05:49 +0500

    send email added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -73,7 +73,7 @@ const Sidebar = () => {
 
       <button
         className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
-        onClick={() => window.open("usmanlatif306@gmail.com")}
+        onClick={() => window.open("mailto:usmanlatif306@gmail.com")}
       >
         Email me
       </button>
